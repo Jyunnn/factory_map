862ad1d5cec50b3a5394f6c643df85758bb6d74e
Jyun Wei <43448066+Jyunnn@users.noreply.github.com>
2025-08-15 15:12:32 +0800

    feat: add header and map layout

--- src/pages/factory_map.vue
+++ src/pages/factory_map.vue
@@ -2,15 +2,32 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <!-- Top row spanning full width -->
+        <v-app-bar flat height="80">
+          <v-app-bar-title>Factory Map</v-app-bar-title>
+        </v-app-bar>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="6" style="flex: 0 0 60%; max-width: 60%;">
-        <!-- Left column 60% -->
+        <iframe
+          allowfullscreen=""
+          height="400"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          src="https://maps.google.com/maps?q=New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          style="border:0;"
+          width="100%"
+        />
       </v-col>
       <v-col cols="6" style="flex: 0 0 40%; max-width: 40%;">
-        <!-- Right column 40% -->
+        <v-row>
+          <v-col cols="6">
+            <!-- Right column first half -->
+          </v-col>
+          <v-col cols="6">
+            <!-- Right column second half -->
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
