88055ddfb5e7116b0e94299374dada0dfa22a546
Jyun Wei <43448066+Jyunnn@users.noreply.github.com>
2025-08-15 13:57:01 +0800

    feat: add factory map page

---
+++ src/pages/factory_map.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <!-- Top row spanning full width -->
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6" style="flex: 0 0 60%; max-width: 60%;">
+        <!-- Left column 60% -->
+      </v-col>
+      <v-col cols="6" style="flex: 0 0 40%; max-width: 40%;">
+        <!-- Right column 40% -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+//
+</script>
