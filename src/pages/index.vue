281437039490a11035e8cf2df0bdbe1b2814d2d7
Jyun Wei <43448066+Jyunnn@users.noreply.github.com>
2025-08-26 17:53:55 +0800

    feat: add D3 tree diagram to homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <HelloWorld />
+  <div>
+    <HelloWorld />
+    <TreeDiagram />
+  </div>
 </template>
 
 <script setup>
-  //
+  import TreeDiagram from '@/components/TreeDiagram.vue'
 </script>
